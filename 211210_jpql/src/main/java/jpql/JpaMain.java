a486576418a5098201cded8783ae47f7c8a48fb9
devboki <octsixxth@gmail.com>
2021-12-10 14:01:55 +0900

    commit

--- 211210_jpql/src/main/java/jpql/JpaMain.java
+++ 211210_jpql/src/main/java/jpql/JpaMain.java
@@ -1,13 +1,15 @@
 package jpql;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-
 public class JpaMain {
-public static void main(String[] args) {	
+	
+	public static void main(String[] args) {	
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpql");
 		EntityManager em = emf.createEntityManager();
@@ -18,8 +20,18 @@ public static void main(String[] args) {
 		try { 
 			Member member = new Member();
 			member.setUsername("member1");
+			member.setAge(10);
 
 			em.persist(member);
+			
+			em.flush();
+			em.clear();
+						
+			List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+								.getResultList();
+			MemberDTO memberDTO = result.get(0);
+			System.out.println("memberDTO.getUsername = " + memberDTO.getUsername());
+			System.out.println("memberDTO.getAge = " + memberDTO.getAge());
 			
 			tx.commit();
 		} catch (Exception e) {
